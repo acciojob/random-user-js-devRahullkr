bdde60c617f4b9db975606e1692c73aa6d4440a7
Rahul Kr <rahulkumar8604745@gmail.com>
2026-02-20 11:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let currenctUser = null;
+let userImage = document.getElementById("userImage")
+let userName = document.getElementById("userName")
+let addintionalInfo = document.getElementById("additionalInfo")
+let getUser = document.getElementById("getUser")
+let infoButtons = document.getElementById("button[data-attr]")
